1e4cd61f8cf7f6ea90d86ac2c1a2857069eedddf
Benjamin West <23172276+bgwest@users.noreply.github.com>
2020-09-18 09:24:00 -0700

    added ability to fire laser cannon

--- m8-games/ship-adventure/Game1.cs
+++ m8-games/ship-adventure/Game1.cs
@@ -11,12 +11,17 @@ namespace ship_adventure
 
         Texture2D Ship;
         Texture2D Background;
+        Texture2D Shot;
 
         SpriteFont Font;
 
         Vector2 ShipPosition;
+        Vector2 ShotPosition;
+        Vector2 ShotOffset;
 
         string MY_TEXT;
+
+        bool HasShot = false;
 
         public Game1()
         {
@@ -38,7 +43,12 @@ namespace ship_adventure
 
             Ship = Content.Load<Texture2D>("blueships1");
             Background = Content.Load<Texture2D>("background");
+            Shot = Content.Load<Texture2D>("shot");
+
             Font = Content.Load<SpriteFont>("defaultFont");
+
+            ShotOffset = new Vector2(Ship.Width / 2, Ship.Height / 2);
+            ShotOffset += new Vector2(60, -24f);
         }
 
         protected override void Update(GameTime gameTime)
@@ -71,7 +81,23 @@ namespace ship_adventure
                 movement.Y += 2;
             }
 
+            if (keystate.IsKeyDown(Keys.Space))
+            {
+                ShotPosition = ShipPosition + ShotOffset;
+                HasShot = true;
+            }
+
             ShipPosition += movement;
+
+            if (HasShot)
+            {
+                ShotPosition.X += 5;
+
+                if (ShipPosition.X > GraphicsDevice.Viewport.Width)
+                {
+                    HasShot = false;
+                }
+            }
 
             base.Update(gameTime);
         }
@@ -82,9 +108,13 @@ namespace ship_adventure
             MY_TEXT = "Space Adventure";
 
             _spriteBatch.Begin();
+
             _spriteBatch.Draw(Background, Vector2.One, Color.White);
             _spriteBatch.Draw(Ship, ShipPosition, Color.White);
+            if (HasShot) _spriteBatch.Draw(Shot, ShotPosition, Color.White);
+
             _spriteBatch.DrawString(Font, MY_TEXT, Vector2.One, Color.Yellow);
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
